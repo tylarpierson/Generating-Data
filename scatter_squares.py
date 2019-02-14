5db6378461b4938183a742f9f72b469acdbf7761
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-02-14 15:44:37 -0500

    added x and y values

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
-plt.scatter(2, 4, s=200)
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+plt.scatter(x_values, y_values, s=200)
 
 # Set the chart title and label axes
 plt.title("Square Numbers", fontsize=24)
