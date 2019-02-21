ec7dde4f3e12fc0f2d484d531886f154a51d5daa
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-02-21 15:58:33 -0500

    looping through points

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
-plt.scatter(x_values, y_values, s=200)
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+
+plt.scatter(x_values, y_values, s=40)
 
 # Set the chart title and label axes
 plt.title("Square Numbers", fontsize=24)
@@ -10,6 +11,9 @@ plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels
-plt.tick_params(axis='both', which='major', labelsize=14)
+# plt.tick_params(axis='both', which='major', labelsize=14)
+
+# Set the range for each axis
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
